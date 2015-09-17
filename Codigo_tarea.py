d35fad31dee7a26965c0ae8321fbe9de23716e18
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-09-17 20:34:42 -0300

    Modificaciones varias al codigo (orden y nombre de las variables) para poder agregar la regla de simpson como metodo de integracion

--- Codigo_tarea.py
+++ Codigo_tarea.py
@@ -7,7 +7,7 @@ wavelength=np.loadtxt('sun_AM0.dat', usecols=[0])
 power=np.loadtxt('sun_AM0.dat', usecols=[1])
 
 #Cambio de unidades
-wavelength*=10 #[Angstrom]
+wavelength*=10 #[A]
 power*=100 #[ergs/(s*cm2*A)]
 
 semilogx(wavelength,power)
@@ -18,18 +18,33 @@ grid(True)
 savefig("Espectro_solar")
 show()
 
-#integral=h*[f(a)+f(b)+sum_fi]
+#Calculo integral: metodo de trapecio y regla de simpson
 n= len(wavelength)-1  #numero de particiones
 a=wavelength[0]
 b=wavelength[n]
-h=(b-a)/(2*n)
-
 fa=power[0]#f(a)
 fb=power[n]#f(b)
-sum_fi=-fa-fb
+h=(b-a)/n
 
-for i in range(n):
-    sum_fi+=power[i]
+#Metodo del trapecio (_t)
+suma_t=-fa-fb  #la sumatoria de los fi no incluye a los terminos extremos
+integral_t=0
 
-integral=h*(fa+fb+(2*sum_fi))
-print integral
+for i in range(len(wavelength)):
+    suma_t+=(2*power[i])
+
+integral_t=(h*(fa+fb+suma_t))/2
+print "Luminosidad total del sol/metodo del trapecio:",integral_t
+
+#Regla de simpson (_s)
+integral_s=0
+suma_s=-fa
+
+for i in range(n): #range(n) para que no llegue al ultimo termino ( f(b) )
+    if(i%2==0):
+        suma_s+=(2*power[i])
+    else:
+        suma_s+=(4*power[i])
+
+integral_s= (h*(suma_s+fa+fb))/3
+print "Luminosidad total del sol/regla de simpson:",integral_s
