9fd1f27c378645bf6ce2983315030517ccc78f72
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-09-20 20:19:44 -0300

    Mejore un poco la visibilidad del codigo para que fuera mas bonito de leer

--- p1.py
+++ p1.py
@@ -1,12 +1,12 @@
 import numpy as np
 from pylab import *
 
-wavelength=np.loadtxt('sun_AM0.dat', usecols=[0])
-power=np.loadtxt('sun_AM0.dat', usecols=[1])
+wavelength = np.loadtxt('sun_AM0.dat', usecols=[0])
+power = np.loadtxt('sun_AM0.dat', usecols=[1])
 
 #Cambio de unidades
-wavelength*=10 #[A]
-power*=100 #[ergs/(s*cm2*A)]
+wavelength*= 10 #[A]
+power*= 100 #[ergs/(s*cm2*A)]
 
 semilogx(wavelength,power)
 xlabel('$Longitud\,de\,onda\,[\AA] $')
