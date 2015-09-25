3210807f2f2fc8dfbf287c6f4324c84ec6bbd268
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-09-25 00:00:37 -0300

    Se agrega el informe

--- p4.py
+++ p4.py
@@ -4,16 +4,13 @@ import scipy.integrate
 wavelength = np.loadtxt('sun_AM0.dat', usecols=[0])
 power = np.loadtxt('sun_AM0.dat', usecols=[1])
 
-#Cambio de unidades
-#wavelength*=10 #[A]
-#power*=100 #[ergs/(s*cm2*A)]
 
-energia=scipy.integrate.trapz(power, wavelength)
-print energia
+energia = scipy.integrate.trapz(power, wavelength)
+print 'integral p2 calculado con scipy',energia
 
-def func(x):
-    '''Retorna la funcion que se desea integrar entre 0 y inf'''
-    return x**3/(np.exp(x)-1)
+def f(x):
+        '''Retorna la funcion que se desea integrar entre 0 y pi/2'''
+        return (((np.tan(x))**3.)*((np.cos(x))**(-2.0)))/(np.exp(np.tan(x))-1)
 
-laotraenergy=scipy.integrate.quad(func,0,np.inf)
-print laotraenergy
+laotraenergy=scipy.integrate.quad(f,0.0001,np.pi/2)[0]
+print 'integral p calculado con scipy',laotraenergy
